201f8f28d663715b573100bc6557cd82287d037c
Jhonny Viana <contactjhonnyviana@gmai.com>
2024-05-26 18:26:21 -0300

    Finishing ContaTerminal.

---
+++ src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo é R$" + saldo);
+        System.out.println("Seu saldo já está disponível para saque.");
+    }
+}
